d475220ea683e7bf396f03af1b36d915425c31ad
Supriyo Das <das.supriyo07@gmail.com>
2024-05-09 22:42:35 +0530

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,3 @@
+document.querySelector("#homeBtn").addEventListener("click", () => {
+  location.href = "/about";
+});
---
+++ public/js/script1.js
@@ -0,0 +1,3 @@
+document.querySelector("#aboutBtn").addEventListener("click", () => {
+  location.href = "/";
+});
